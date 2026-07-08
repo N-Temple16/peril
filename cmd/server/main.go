944730331e4276bfbd61df2fdd3be0f4f30ebff2
N-Temple16 <nigelatemple@gmail.com>
2026-07-08 13:53:47 -0400

    MQTT and STOMP added along with exchanges and queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+
+	newCon, err := amqp.Dial(connectionStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer newCon.Close()
+	fmt.Println("Peril game server successfully connected to RabbitMQ")
+
+	ch, err := newCon.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open channel: %v", err)
+	}
+	defer ch.Close()
+
+	err = pubsub.PublishJSON(
+		ch, 
+		routing.ExchangePerilDirect, 
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Fatalf("Failed to publish JSON: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed")
 }
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,30 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	jsonData, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("error marshaling JSON: %w", err)
+	}
+
+	return ch.PublishWithContext(
+		context.Background(), 
+		exchange, 
+		key, 
+		false, 
+		false, 
+		amqp.Publishing{
+			ContentType: "application/json", 
+			Body: jsonData,
+		},
+	)
+}
+
+	
